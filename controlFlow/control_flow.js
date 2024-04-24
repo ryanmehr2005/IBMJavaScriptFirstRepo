56f599480d26162b32048af740f95bd7d6ce95a2
Ryan Mehr <ryanmehr2005@gmail.com>
2024-04-23 22:12:21 -0400

    this is the message

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,18 @@
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User Category:", userCategory);
