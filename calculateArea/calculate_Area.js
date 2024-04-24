de3d6c95c8b2f8dab7c63f5f9dbe2f2d5c712749
Ryan Mehr <ryanmehr2005@gmail.com>
2024-04-24 15:13:36 -0400

    the message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -10,3 +10,18 @@ function calculateArea()
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateTotal()
+{
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let grocerytotal = grocery1 + grocery2 + grocery3;
+
+    document.getElementById('groceryresult').innerText = `The total amount is: $${grocerytotal}`;
+}
