70dbf97e3c960eb9fb31619204af14320ada1052
Ryan Mehr <ryanmehr2005@gmail.com>
2024-04-26 19:50:13 -0400

    Finished speed analysis task

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -38,8 +38,10 @@ function endTest()
         wpm = Math.round((typedWords / timeElapsed) * 60);
     }
 
+    var totalLength = userTypedText.length;
+
     var outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = "<h2>Typing Test Results: </h2>" + "<p>Words Typed: " + typedWords + "</p>" +
+    outputDiv.innerHTML = "<h2>Typing Test Results: </h2>" + "<p>Total Length: " + totalLength + "</p>"+ "<p>Words Typed: " + typedWords + "</p>" +
     "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
     "<p>Words Per Minute (WPM): " + wpm + "</p>";
 
